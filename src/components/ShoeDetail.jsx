dd25f6f1c2bd4bb47c5a7a4b840818adf13b0b0b
Your Name <urielparavi75@gmail.com>
2025-05-13 09:46:30 +0300

    19. Shoe detail layout

---
+++ src/components/ShoeDetail.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+import nike1 from '../assets/n1-min.png';
+
+export function ShoeDetail() {
+  return (
+    <div className="flex flex-col space-y-4 lg:flex-row lg:flex-row-reverse">
+      <div className="flex-1">
+        <img src={nike1} alt="The red nike shoe" />
+      </div>
+      <div className="flex-1 space-y-6">
+        <div className="text-5xl font-black md:text-9xl">Nike Air max 270</div>
+        <div className="font-medium md:text-xl">
+          {
+            "The Nike Air Max 270 is a lifestyle shoe that's sure to turn heads with its vibrant color gradient."
+          }
+        </div>
+        <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
+        <div className="space-x-10">
+          <button className="h-14 w-44 bg-black text-white hover:bg-gray-800 active:bg-gray-700 rounded-lg">
+            Add to bag
+          </button>
+          <Link className="text-lg font-bold underline underline-offset-4">
+            View Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
